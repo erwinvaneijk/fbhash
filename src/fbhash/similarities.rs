0d375a55119ab78435354c0a7b9807e6446547d1
Erwin J. van Eijk <235739+erwinvaneijk@users.noreply.github.com>
2023-07-30 21:23:30 +0200

    Fix clippy warning

--- src/fbhash/similarities.rs
+++ src/fbhash/similarities.rs
@@ -275,7 +275,6 @@ pub fn ranked_search(
     let mut result = Vec::new();
     queue
         .into_sorted_iter()
-        .into_iter()
         .for_each(|(doc, similarity)| {
             result.push((similarity.into_inner(), doc.clone()));
         });

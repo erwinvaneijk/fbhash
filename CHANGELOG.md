218b9f2ef2c63260c8172d021a14eb3b8f698d33
Erwin van Eijk <235739+erwinvaneijk@users.noreply.github.com>
2025-06-25 12:35:50 +0200

    refactor(license): :memo: Update license to EUPL-1.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,10 +1,20 @@
-0.1.4
-=====
+ChangeLog
+=========
+
+Version 0.1.6
+-------------
+
+Relicensed to EUPL-1.2
+
+Version 0.1.4
+-------------
+
 Updated the dependencies to no longer use the vulnerable version of
 zerocopy.
 
-0.1.3
-=====
+Version 0.1.3
+-------------
+
 Updated all the dependencies.
 Updated the copyright notice.
 Bumped the version number.
--- src/fbhash/mod.rs
+++ src/fbhash/mod.rs
@@ -1,3 +1,12 @@
+// This file is part of fbhash
+//
+// Copyright (C) 2025 Erwin J. van Eijk
+//
+// Licensed under the EUPL, Version 1.2 or – as soon they will be approved by the European Commission - subsequent versions of the EUPL.
+// You may not use this work except in compliance with the License.
+// You may obtain a copy of the License at:
+// https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+
 pub mod chunker;
 
 pub mod similarities;
